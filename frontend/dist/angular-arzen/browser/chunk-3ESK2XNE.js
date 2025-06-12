017adb127126774a8b9178d19398517e95107429
Zoltan Beke <zoltan.beke89@gmail.com>
2025-06-12 11:03:22 +0200

    Dist folder of frontend copied

---
+++ frontend/dist/angular-arzen/browser/chunk-3ESK2XNE.js
@@ -0,0 +1,1 @@
+import{J as s,K as o}from"./chunk-GAFZRVCH.js";var r=o("responsiblePerson"),n=s(r,e=>e.responsiblePerson),l=s(r,e=>e.responsiblePersons);export{n as a,l as b};
---
+++ frontend/dist/angular-arzen/browser/chunk-VQRAC7UO.js
@@ -0,0 +1,1 @@
+import{J as t,K as o}from"./chunk-GAFZRVCH.js";var c=o("company"),p=t(c,e=>e.company),r=t(c,e=>e.companies);export{p as a};
